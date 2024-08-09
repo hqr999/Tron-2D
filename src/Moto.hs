dce87bad456d30ff90f6aebd852a2b010a96fb4a
Henrique Queiroz Reuter <henriquereuter46@gmail.com>
2024-08-08 22:41:25 -0300

    Mudei a cor da moto do jogador

--- src/Moto.hs
+++ src/Moto.hs
@@ -9,13 +9,16 @@ area :: Float
 area = 18.0
 
 -- O quadrado inicial que será a moto é desenhado por essa funçaõ que recebe 3 Floats--
+--Também mudaremos a cor da moto para a corMotoJogador--
 desenhaQ :: Float -> Picture
-desenhaQ area1 = rectangleSolid area1 area1
+desenhaQ area1 = color corMotoJogador $ rectangleSolid area1 area1
 
 -- A função quadrado pega o resultado de desnhaQ e coloca em pictures 
 -- que tem uma assinatura [Picture] -> Picture
 quadrado :: Picture
 quadrado = pictures [desenhaQ area]
+
+ 
 
 
 
